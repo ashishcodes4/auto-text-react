689b51f61d373af693a077f52f786a8ce9babdd7
Ashish <ashishcodes4@gmail.com>
2018-11-30 20:22:16 +0530

    working on UI selecion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        
+         
       </div>
     );
   }
---
+++ src/Components/Dropdown.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dropdown extends Component {
+    
+  render() {
+
+
+    return (
+        <div></div>
+    )
+  }
+}
+
+export default Dropdown;
